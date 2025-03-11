dce2316327a7ecd416ca3cb76a0eebaae3773c22
2387926 <2387926@LTIN523305>
2025-03-11 10:17:46 +0530

    delivery payment services added

--- OnlineFoodDeliverySystem/Controllers/CustomersController.cs
+++ OnlineFoodDeliverySystem/Controllers/CustomersController.cs
@@ -1,26 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using OnlineFoodDeliverySystem.Data;
 
-namespace OnlineFoodDeliverySystem.Controllers
+[ApiController]
+[Route("api/customers")]
+public class CustomerController: ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomersController : ControllerBase
-    {
-        private readonly FoodDbContext _context;
-
-        public CustomersController(FoodDbContext context)
-        {
-            _context = context;
-        }
-        //Register
-        [HttpPost]
-        public void RegisterCustomer([FromBody] Customer customer)
-        {
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
-        }
-
-    }
+    private readonly ICustomer
 }
---
+++ OnlineFoodDeliverySystem/Serivces/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using OnlineFoodDeliverySystem.Models;
+
+namespace OnlineFoodDeliverySystem.Serivces
+{
+    public interface IPaymentService
+    {
+        List<Payment> GetPayments();
+        Payment GetPaymentById(int id);
+        Payment GetPaymentByMethod(string method);
+        void AddPayment(Payment payment);
+        void UpdatePaymentStatus(int id, string status);
+        void DeletePayment(int id);
+    }
+}
