a7e0d74b135c773bdadaac515483e3dd7568e0ce
Namepally Shanmukh <2388121@cognizant.com>
2025-03-13 10:26:54 +0530

    Exception Handler Attribute Added

---
+++ OnlineFoodDeliverySystem/Exceptions/ExceptionHandlerAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineFoodDeliverySystem.Exceptions
+{
+
+    public class ExceptionHandlerAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exceptionType = context.Exception.GetType();
+            var message = context.Exception.Message;
+
+            if (exceptionType == typeof(NotFoundException))
+            {
+                var result = new NotFoundObjectResult(message);
+                context.Result = result;
+            }
+            else if (exceptionType == typeof(AlreadyExistsException))
+            {
+                var result = new ConflictObjectResult(message);
+                context.Result = result;
+            }
+            else
+            {
+                var result = new StatusCodeResult(500);
+                context.Result = result;
+            }
+        }
+    }
+}
